8bdf8d56cfa69500ee5e80f1c6e8e356a4197d91
Carlos Barauna <cbarauna@MacBook-Air-de-Carlos.local>
2019-06-14 00:20:20 -0300

    Front End

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import Header from "./components/Header";
+import Routes from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes />
+    </BrowserRouter>
   );
 }
 
